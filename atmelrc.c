6b60714e4e53372c5ba2f07e1840e076b67621f9
Anton DahlstrÃ¶m <anton.dahlstrom508@gmail.com>
2016-05-11 16:38:16 +0200

    Implemented most of Timer1 and result translation
    Timer1 now funcitons as a timekeeper when the signals are received.
    This is not yet tested in a real setting, so there might be issues.

--- atmelrc.c
+++ atmelrc.c
@@ -1,13 +1,16 @@
 #include <atmelrc.h>
 
-uint8_t pb3_state = 0;
-uint8_t pb4_state = 0;
+uint8_t signalA = 0;
+uint8_t signalB = 0;
 
 uint16_t counterA = 0;
 uint16_t counterB = 0;
 
-uint8_t PWM_outA = 127;
-uint8_t PWM_outB = 0;
+int16_t inputA_value = 0;
+int16_t inputB_value = 0;
+
+uint16_t PWM_outA = 127;
+uint16_t PWM_outB = 0;
 
 int main (void) {
 
@@ -15,8 +18,57 @@ int main (void) {
 
 	while(1){
 
-		//This loop is continuously running
-	
+
+		if( counterA && !signalA ){
+			//If the counter has measured a time, and the signal is no longer present
+			inputA_value = counterA;
+			counterA = 0;
+		}
+
+		if( counterB && !signalB ){
+			//If the counter has measured a time, and the signal is no longer present
+			inputB_value = counterB;
+			counterB = 0;
+		}
+
+
+		//Calculates the values of the PWM 8-bit output signals 255 * (value-1000)/1000
+		PWM_outA = CONVERSIONFACTOR * ( inputA_value - PWMLOWLEVEL );
+		PWM_outB = CONVERSIONFACTOR * ( inputB_value - PWMLOWLEVEL );
+
+
+		// Set PWM output for channel A
+		if( PWM_outA > 255) {
+
+			OCR0A = 255;
+
+		} else if ( PWM_outA < 0 ){
+
+			OCR0A = 0;
+
+		} else {
+
+			OCR0A = (uint8_t) PWM_outA;
+
+		}
+
+		// Set PWM output for channel B
+		if( PWM_outB > 255) {
+
+			OCR0B = 255;
+
+		} else if ( PWM_outB < 0 ){
+
+			OCR0B = 0;
+
+		} else {
+
+			OCR0B = (uint8_t) PWM_outB;
+
+		}
+
+
+
 	}
 
 }
@@ -25,22 +77,16 @@ void setup(void){
 
 	cli(); //Disable interrupts temoprarily
 
+
 	//Internal Crystal calibration
-	OSCCAL = 0b01011000;
+		OSCCAL = 0b01011000;
+
 
 	//Setup of the two input pins, adding interrupts
-	
-		//Interrupts
-		// GIMSK |= _BV(PCIE);
-		// PCMSK = _BV(PCINT3) | _BV(PCINT4);
+		GIMSK |= _BV(PCIE);
+		PCMSK = _BV(PCINT3) | _BV(PCINT4);
 
-		//Set pin7/PB2 as output, with high level
-		// DDRB |= _BV(DDB2);
-		// PORTB |= _BV(PB2);
-
-
-	//Setup of Timer1 for time-keeping
-
+		
 	//Setup of Timer0 for PWM modes
 		TCCR0B |= _BV(CS01); //Set clock to CLK/8, activating PWM
 		TCCR0A |= _BV(WGM00); //Set Phase-correct PWM mode
@@ -48,79 +94,53 @@ void setup(void){
 		OCR0B = PWM_outB; //Set the inital compare levels for outB
 		DDRB |= _BV(DDB0) | _BV(DDB1); // Set pins as output
 		TCCR0A |= _BV(COM0A1) | _BV(COM0B1); //Set pins as PWM outputs
-		TIMSK |= _BV(TOIE0); //Enable interrupts
-		
+		// TIMSK |= _BV(TOIE0); //Enable overflow interrupt
+
+
+	//Setup of Timer1 for time-keeping
+		PLLCSR |= _BV(PLLE);
+		while( (PLLCSR & _BV(PLOCK)) == 0 ); //Wait for PLL to lock in
+		PLLCSR |= _BV(PCKE);
+		TCCR1 |= _BV(CS12); //Set clock to PCK/8, giving a resolution of 8 counts per µs
+		TCCR1 |= _BV(CTC1); //Reset timer after OCR1C match
+		OCR1C = 8; //Number of counts for 1 µs
+		TIMSK |= _BV(TOIE1);//Enable overflow interrupt
+
 
 	sei(); //Enable interrupts again
 
 }
 
+ISR( TIMER1_OVF_vect ){
+
+	//Function to calculate µs of the signal
+	cli();
+	if(signalA) {
+		counterA++; //Add another 1µs to the counter. 
+	} else if(signalB){
+		counterB++;
+	}
+	sei();
+
+}
+
 ISR( TIMER0_OVF_vect ){
 
-	counterA++;
+/*	counterA++;
 	if(counterA > 50){
 		counterA = 0;
 		OCR0A = OCR0A + 1;
 		OCR0B = OCR0B + 1;
-	}
-
-	// PORTB ^= ~(_BV(PB4)); //Toggle PB4
+	}*/
 
 }
 
-ISR( TIMER0_COMPA_vect ) {
-
-	counterA++;
-
-	if(counterA > 10000){
-		counterA = 0;
-		PWM_outA += 10;
-		OCR0A = _BV(PWM_outA);
-	}
-
-}
-
-ISR( TIMER0_COMPB_vect ) {
-
-	counterB++;
-
-	if(counterB > 10000){
-		counterB = 0;
-		PWM_outB += 10;
-		OCR0B = _BV(PWM_outB);
-	}
-
-}
-
-//Interrupt vectors
 ISR( PCINT0_vect ) {
+
+	//Sets signal values
+	signalA = (PINB>>PINB3) & 1;
+	signalB = (PINB>>PINB4) & 1;
 	
-	//To test: Toggle the corresponding ports of input/output
-
-	// PORTB |= _BV(PB0) | _BV(PB1);
-/*
-	if( (PINB>>PINB3) & ~(pb3_state) ){
-		//IF PB3 has changed
-
-
-		//record new state of pb3
-		pb3_state = (PINB>>PINB3) & _BV(1);
-
-		//Toggle pin 0
-		PORTB ^= _BV(PB0);
-
-	} else if ( (PINB>>PINB4) & ~(pb4_state) ){
-		//IF PB4 has changed
-
-		//record new state of pb4
-		pb4_state = (PINB>>PINB4) & _BV(1);
-
-		//Toggle pin 1
-		PORTB ^= _BV(PB1);
-
-	}
-
-*/
-
 }
+
 
--- atmelrc.h
+++ atmelrc.h
@@ -1,11 +1,16 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-//Includes
-#include <avr/io.h>
-#include <avr/interrupt.h>
+	//Includes
+	#include <avr/io.h>
+	#include <avr/interrupt.h>
 
-//Function delcarations
-void setup(void);
+	//Function delcarations
+	void setup(void);
+
+	//Cosntants
+	#define PWMLOWLEVEL (uint16_t) 1000 //The maximum value of the PWM signal
+	#define PWMHIGHLEVEL (uint16_t) 2000 //The minimum value of the PWM signal
+	#define CONVERSIONFACTOR (float) (255/PWMLOWLEVEL)
 
 #endif
